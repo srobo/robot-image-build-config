9bc8114afcf83cdffa0cc39809b7afe919ee5603
Karina Kwiatek <kkwiatek@studentrobotics.org>
2022-05-22 13:00:55 +0200

    Update to kirkstone and retarget to Raspberry Pi 4

--- scripts/assemble.sh
+++ scripts/assemble.sh
@@ -35,8 +35,7 @@ if [ ! -d outputs ]; then
     mkdir -p outputs
 fi
 
-# Check if latest already exists ??
-cp tmp/deploy/images/odroid-u3/srobo-image-robot-odroid-u3.wic "outputs/${1}.img"
+bmaptool copy --bmap srobo-image-debug-raspberrypi4-64.wic.bmap srobo-image-debug-raspberrypi4-64.wic.bz2 "outputs/${1}.img"
 
 # Create loopback device and mount it
 if [ ! -d imagemnt ]; then
@@ -46,21 +45,6 @@ fi
 loop_dev=$(losetup --show -P -f "outputs/${1}.img")
 mount ${loop_dev}p1 imagemnt/
 echo "Mounted image"
-echo ""
-
-# Swap out kernel bits
-echo "Deleting Yocto kernel"
-rm -rf imagemnt/boot/*
-
-echo "Copying boot stuff"
-cp -r kernel-files/boot/* imagemnt/boot
-
-echo "Removing old modules"
-rm -rf imagemnt/lib/modules/*
-
-echo "Copying modules"
-cp -r kernel-files/modules/* imagemnt/lib/modules
-
 echo ""
 
 # Set the astroia vesion
@@ -73,15 +57,6 @@ echo ""
 echo "Unmounting"
 umount imagemnt
 
-echo ""
-
-# Run the fuse script on the sd image
-echo "Fusing"
-pushd kernel-files/boot
-bash ./sd_fusing.sh $loop_dev
-popd
-losetup -d $loop_dev
-echo "Fuse complete"
 echo ""
 
 # Create output image
--- setup-env.sh
+++ setup-env.sh
@@ -3,7 +3,7 @@ if [ ! -d build/conf ]; then
     mkdir -p build/conf
 fi
 
-for conf in odroid-u3-config/*; do
+for conf in srobo-config/*; do
     config="$(basename $conf)"
     if [ ! -e "build/conf/$config" ]; then
         echo "Creating symlink for $conf to build/conf/$config"
@@ -15,8 +15,5 @@ if [ ! -e build/assemble.sh ]; then
     echo "Creating symlink for scripts/assemble.sh in build/assemble"
     ln -s "../scripts/assemble.sh" "build/assemble"
 fi
-
-echo "Copying Arch kernel bits into build dir"
-cp -r kernel-files build/
 
 source sources/poky/oe-init-build-env build

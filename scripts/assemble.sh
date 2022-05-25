9c86a2965f1c73aca96f2cb126283dd6b604e1e6
Karina Kwiatek <kkwiatek@studentrobotics.org>
2022-05-25 19:19:05 +0200

    Allow assemble script to flash block devices

--- scripts/assemble.sh
+++ scripts/assemble.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Setup process
-# copy kernel-files to build dir
-# symlink bake script to build dir
-
 set -e
 
 if [ "$EUID" -ne 0 ]; then
@@ -16,17 +12,23 @@ if [ $# -ne 2 ]; then
     exit
 fi
 
-echo "Output filename will be: $1.img.xz"
+if [ -b "$1" ]; then
+    echo "Output will be flashed to $1"
+else
+    echo "Output filename will be: $1.img.xz"
+fi
 echo "Version string will be: $2"
 echo ""
 
-echo "Is this correct"
-select yn in "Yes" "No"; do
-    case $yn in
-        "Yes") break ;;
-        *) exit ;;
-    esac
-done
+if [ ! -b "$1" ]; then
+    echo "Is this correct"
+    select yn in "Yes" "No"; do
+        case $yn in
+            "Yes") break ;;
+            *) exit ;;
+        esac
+    done
+fi
 
 echo ""
 
@@ -35,33 +37,54 @@ if [ ! -d outputs ]; then
     mkdir -p outputs
 fi
 
-bmaptool copy --bmap srobo-image-debug-raspberrypi4-64.wic.bmap srobo-image-debug-raspberrypi4-64.wic.bz2 "outputs/${1}.img"
-
-# Create loopback device and mount it
-if [ ! -d imagemnt ]; then
-    mkdir -p imagemnt
+script_dir="$(dirname "$(readlink -f "$0")")"
+image_dir="$script_dir/../build/tmp/deploy/images/raspberrypi4-64"
+if [ -b "$1" ]; then
+    output_path="$1"
+else
+    output_path="outputs/${1}.img"
 fi
 
-loop_dev=$(losetup --show -P -f "outputs/${1}.img")
-mount ${loop_dev}p1 imagemnt/
-echo "Mounted image"
+bmaptool copy --bmap "$image_dir/srobo-image-robot-raspberrypi4-64.wic.bmap" "$image_dir/srobo-image-robot-raspberrypi4-64.wic.bz2" "$output_path"
+
+mount_path="$script_dir/../imagemnt"
+if [ ! -d "$mount_path" ]; then
+    mkdir -p "$mount_path"
+fi
+
+if [ -b "$1" ]; then
+    # Mount the rootfs partition of our target block device
+    mount "${1}2" "$mount_path"
+    echo "Mounted rootfs"
+else
+    # Create loopback device and mount it
+
+    loop_dev=$(losetup --show -P -f "outputs/${1}.img")
+    mount ${loop_dev}p1 "$mount_path"
+    echo "Mounted image"
+fi
 echo ""
 
 # Set the astroia vesion
 echo "Updating the version number in astoria.toml"
-sed -i "s/%VERSIONSTRING%/$2/g" imagemnt/etc/astoria.toml
+sed -i "s/%VERSIONSTRING%/$2/g" "$mount_path/etc/astoria.toml"
 
 echo ""
 
 # Unmount image
 echo "Unmounting"
-umount imagemnt
+umount "$mount_path"
 
 echo ""
 
-# Create output image
-echo "Compressing output image"
-xz -v "outputs/${1}.img"
+if [ -b "$1" ]; then
+    echo "Syncing disks"
+    sync
+else
+    # Create output image
+    echo "Compressing output image"
+    xz -v "$output_path"
 
-echo ""
-echo "Image created at: outputs/${1}.img.xz"
+    echo ""
+    echo "Image created at: $output_path.xz"
+fi

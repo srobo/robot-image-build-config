ce490c48432b1b5246579e3b1cbb6ff2a208bb8e
Karina Kwiatek <kkwiatek@studentrobotics.org>
2022-08-01 19:46:47 +0200

    Add RAUC

--- scripts/assemble.sh
+++ scripts/assemble.sh
@@ -7,7 +7,7 @@ if [ "$EUID" -ne 0 ]; then
     exit
 fi
 
-if [ $# -ne 2 ]; then
+if [ $# -ne 1 ]; then
     echo "Usage: $0 <output filename> <version string>"
     exit
 fi
@@ -17,7 +17,6 @@ if [ -b "$1" ]; then
 else
     echo "Output filename will be: $1.img.xz"
 fi
-echo "Version string will be: $2"
 echo ""
 
 if [ ! -b "$1" ]; then
@@ -45,37 +44,7 @@ else
     output_path="outputs/${1}.img"
 fi
 
-bmaptool copy --bmap "$image_dir/srobo-image-robot-raspberrypi4-64.wic.bmap" "$image_dir/srobo-image-robot-raspberrypi4-64.wic.bz2" "$output_path"
-
-mount_path="$script_dir/../imagemnt"
-if [ ! -d "$mount_path" ]; then
-    mkdir -p "$mount_path"
-fi
-
-if [ -b "$1" ]; then
-    # Mount the rootfs partition of our target block device
-    mount "${1}2" "$mount_path"
-    echo "Mounted rootfs"
-else
-    # Create loopback device and mount it
-
-    loop_dev=$(losetup --show -P -f "outputs/${1}.img")
-    mount ${loop_dev}p1 "$mount_path"
-    echo "Mounted image"
-fi
-echo ""
-
-# Set the astroia vesion
-echo "Updating the version number in astoria.toml"
-sed -i "s/%VERSIONSTRING%/$2/g" "$mount_path/etc/astoria.toml"
-
-echo ""
-
-# Unmount image
-echo "Unmounting"
-umount "$mount_path"
-
-echo ""
+bmaptool copy --nobmap "$image_dir/srobo-image-robot-raspberrypi4-64.wic.bz2" "$output_path"
 
 if [ -b "$1" ]; then
     echo "Syncing disks"

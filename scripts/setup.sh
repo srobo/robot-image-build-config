08fa36fa7f1abaf719ac7d8d2ab2fcc1060e8e54
Dan Trickey <git@trickey.io>
2022-09-17 19:06:08 +0100

    Remove setup script

--- scripts/setup.sh
+++
@@ -1,5 +0,0 @@
-sudo apt -y install gawk wget git-core diffstat unzip texinfo gcc-multilib \
-     build-essential chrpath socat cpio python python3 python3-pip python3-pexpect \
-     xz-utils debianutils iputils-ping libsdl1.2-dev xterm zstd bmap-tools
-
-git submodule update --init --recursive

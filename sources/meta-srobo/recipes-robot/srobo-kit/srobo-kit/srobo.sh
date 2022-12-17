e5ee49fd5911ab61b1375df5afee5baca25f65a6
WillB97 <will@rudi.org.uk>
2022-12-17 17:48:30 +0000

    Set opencv gstreamer environment variable for all sessions

--- sources/meta-srobo/recipes-robot/srobo-kit/srobo-kit/srobo.sh
+++ sources/meta-srobo/recipes-robot/srobo-kit/srobo-kit/srobo.sh
@@ -1,2 +1,5 @@
 # Add sbin to the path for non-root users so that root-only commands like shutdown/ip can be used through sudo
 PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin
+
+# Stop OpenCV using gstreamer in all sessions
+export OPENCV_VIDEOIO_PRIORITY_GSTREAMER=0

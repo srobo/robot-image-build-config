34d805ea2187c936f07a77e2a753e0daf55187ad
WillB97 <will@rudi.org.uk>
2023-08-26 21:56:18 +0100

    Remove unneeded step

--- scripts/090-webui.sh
+++ scripts/090-webui.sh
@@ -24,6 +24,3 @@ chmod -R 755 /usr/share/kit-ui
 # Set config
 rm /etc/nginx/sites-enabled/default
 mv /tmp/packer/webui/kit-ui.nginx /etc/nginx/sites-enabled/
-
-# fix a bug where nginx's log directory doesn't exist
-mkdir -p /var/log/nginx

607f6fb93d577031ff203174949ccbed5c1569d7
WillB97 <will@rudi.org.uk>
2024-08-18 14:16:25 +0100

    Remove faulty rpi.gpio-common package
    There's bug with the rpi.gpio-common udev rule that causes the root hub to be made only readable by root when the motor board is plugged in too far down the USB tree.
    Seen in SR2024 with motor boards connected to 7 port USB hubs

--- scripts/030-system.sh
+++ scripts/030-system.sh
@@ -20,3 +20,8 @@ echo 'VIDEO_CAMERA = "1"' >> /boot/config.txt
 # Disable using gstreamer for opencv in profile.d
 mv /tmp/packer/system/srobo-profile.sh /etc/profile.d/srobo.sh
 chmod 755 /etc/profile.d/srobo.sh
+
+# Remove a buggy udev package that breaks the USB tree if FTDI chips are plugged into too many USB hubs
+# See https://github.com/raspberrypi/linux/issues/3779#issuecomment-709481662
+# and https://groups.google.com/g/linux.debian.bugs.dist/c/5jI9dDZgfUU
+apt-get remove -y rpi.gpio-common

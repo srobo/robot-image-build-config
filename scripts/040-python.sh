f63abb8b8994e3812530c286c8a164620c0c9801
Jake Howard <git@theorangeone.net>
2024-08-25 22:24:37 +0100

    Ensure user has ownership of venv dir

--- scripts/040-python.sh
+++ scripts/040-python.sh
@@ -8,6 +8,10 @@ apt-get -y install \
     cmake \
     build-essential
 
+# Create directory for virtualenv
+mkdir /opt/venv
+chown $user /opt/venv
+
 chmod +x /tmp/packer/python/install-python.sh /tmp/packer/python/install-packages.sh
 su - $user -c /tmp/packer/python/install-python.sh
 su - $user -c /tmp/packer/python/install-packages.sh
